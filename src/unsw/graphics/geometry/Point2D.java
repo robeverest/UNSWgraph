8aea9cd0645cae448add4c8af064b32a7d5923ad
Rob Everest <rob.everest@gmail.com>
2018-07-09 17:35:40 +1000

    Make buffer naming clearer

--- src/unsw/graphics/geometry/Point2D.java
+++ src/unsw/graphics/geometry/Point2D.java
@@ -1,10 +1,7 @@
 package unsw.graphics.geometry;
 
-import java.nio.IntBuffer;
-
 import com.jogamp.opengl.GL;
 import com.jogamp.opengl.GL3;
-import com.jogamp.opengl.util.GLBuffers;
 
 import unsw.graphics.Point2DBuffer;
 
@@ -39,17 +36,17 @@ public class Point2D {
     public void draw(GL3 gl) {
         Point2DBuffer buffer = new Point2DBuffer(1);
         buffer.put(0, this);
-        IntBuffer bufIds = GLBuffers.newDirectIntBuffer(1);
-        gl.glGenBuffers(1, bufIds);
+        int[] names = new int[1];
+        gl.glGenBuffers(1, names, 0);
         
-        gl.glBindBuffer(GL.GL_ARRAY_BUFFER, bufIds.get(0));
+        gl.glBindBuffer(GL.GL_ARRAY_BUFFER, names[0]);
         
         gl.glBufferData(GL.GL_ARRAY_BUFFER, 2*Float.BYTES, buffer.getBuffer(), GL.GL_STATIC_DRAW);
         
         gl.glVertexAttribPointer(0, 2, GL.GL_FLOAT, false, 0, 0);
         gl.glDrawArrays(GL.GL_POINTS, 0, 1);
         
-        gl.glDeleteBuffers(1, bufIds);
+        gl.glDeleteBuffers(1, names, 0);
         
     }
 
