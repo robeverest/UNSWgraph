79f2b9a0d4c3f2ca68a7dd03f9e368ad5426a274
Rob Everest <rob.everest@gmail.com>
2018-07-28 15:23:41 +1000

    Wibble vertices in head of person example

--- src/unsw/graphics/examples/person/Head.java
+++ src/unsw/graphics/examples/person/Head.java
@@ -17,15 +17,15 @@ import unsw.graphics.geometry.Polygon2D;
  */
 public class Head {
 
-    private static final int NPOINTS = 20;
+    private static final int VERTICES = 32;
     private static final float RADIUS = 3;
     
     private Polygon2D poly;
     
     public Head() {
         List<Point2D> points = new ArrayList<Point2D>();
-        for (int i = 0; i < NPOINTS; i++) {
-            float a = (float) (i * Math.PI * 2 / NPOINTS); // java.util.Math uses radians!!!
+        for (int i = 0; i < VERTICES; i++) {
+            float a = (float) (i * Math.PI * 2 / VERTICES); // java.util.Math uses radians!!!
             float x = RADIUS * (float) Math.cos(a);
             float y = RADIUS * ((float) Math.sin(a) + 1); // Off center
             Point2D p = new Point2D(x, y);
