e530aebfa949326523a0011dd3bbd3f5d8c08501
Rob Everest <rob.everest@gmail.com>
2018-08-02 17:55:33 +1000

    Mandelbrot starter code

---
+++ shaders/vertex_mandelbrot.glsl
@@ -0,0 +1,19 @@
+
+// Incoming vertex position
+in vec2 position;
+
+uniform mat3 model_matrix;
+
+uniform mat3 view_matrix;
+
+void main() {
+	// The global position is in homogenous coordinates
+    vec3 globalPosition = model_matrix * vec3(position, 1);
+
+    // The position in camera coordinates
+    vec3 viewPosition = view_matrix * globalPosition;
+
+    // We must convert from a homogenous coordinate in 2D to a homogenous
+    // coordinate in 3D.
+    gl_Position = vec4(viewPosition.xy, 0, 1);
+}
