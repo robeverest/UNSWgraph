4727f59a2d6b3c58e0ac2792353bc6ba7fa738bd
Rob Everest <rob.everest@gmail.com>
2019-07-05 14:36:40 +1000

    Add support for textures to TriangleMesh and ModelViewer

--- src/unsw/graphics/examples/ModelViewer.java
+++ src/unsw/graphics/examples/ModelViewer.java
@@ -3,12 +3,14 @@ package unsw.graphics.examples;
 import java.awt.Color;
 import java.io.IOException;
 
+import com.jogamp.opengl.GL;
 import com.jogamp.opengl.GL3;
 
 import unsw.graphics.Application3D;
 import unsw.graphics.CoordFrame3D;
 import unsw.graphics.Matrix4;
 import unsw.graphics.Shader;
+import unsw.graphics.Texture;
 import unsw.graphics.geometry.Point3D;
 import unsw.graphics.geometry.TriangleMesh;
 
@@ -30,16 +32,20 @@ public class ModelViewer extends Application3D {
 
     private static final boolean USE_LIGHTING = true;
 
+    private static final boolean USE_TEXTURE = true;
+
     private float rotateY;
 
     private TriangleMesh model;
 
     private TriangleMesh base;
 
+    private Texture texture;
+
     public ModelViewer() throws IOException {
         super("Model viewer", 600, 600);
-        model = new TriangleMesh("res/models/bunny.ply", true);
-        base = new TriangleMesh("res/models/cube_normals.ply", true);
+        model = new TriangleMesh("res/models/bunny.ply", true, true);
+        base = new TriangleMesh("res/models/cube_normals.ply", true, true);
     }
 
     @Override
@@ -47,11 +53,24 @@ public class ModelViewer extends Application3D {
         super.init(gl);
         model.init(gl);
         base.init(gl);
-        if (USE_LIGHTING) {
-            Shader shader = new Shader(gl, "shaders/vertex_phong.glsl",
-                    "shaders/fragment_phong.glsl");
-            shader.use(gl);
+        if (USE_TEXTURE) {
+            texture = new Texture(gl, "res/textures/BrightPurpleMarble.png", "png", false);
         }
+
+        Shader shader = null;
+        if (USE_LIGHTING && USE_TEXTURE) {
+            shader = new Shader(gl, "shaders/vertex_tex_phong.glsl",
+                    "shaders/fragment_tex_phong.glsl");
+        } else if (USE_LIGHTING) {
+            shader = new Shader(gl, "shaders/vertex_phong.glsl",
+                    "shaders/fragment_phong.glsl");
+        } else if (USE_TEXTURE) {
+            shader = new Shader(gl, "shaders/vertex_tex_3d.glsl",
+                    "shaders/fragment_tex_3d.glsl");
+        } else {
+            shader = new Shader(gl, "shaders/vertex_3d.glsl", "shaders/fragment_3d.glsl");
+        }
+        shader.use(gl);
     }
 
     @Override
@@ -70,6 +89,18 @@ public class ModelViewer extends Application3D {
     public void display(GL3 gl) {
         super.display(gl);
 
+        //Set the texture if we're using it.
+        if (USE_TEXTURE) {
+            Shader.setInt(gl, "tex", 0);
+
+            gl.glActiveTexture(GL.GL_TEXTURE0);
+            gl.glBindTexture(GL.GL_TEXTURE_2D, texture.getId());
+
+            Shader.setPenColor(gl, Color.WHITE);
+        } else {
+            Shader.setPenColor(gl, new Color(0.5f, 0.5f, 0.5f));
+        }
+
         // Compute the view transform
         CoordFrame3D view = CoordFrame3D.identity().translate(0, 0, -2)
                 // Uncomment the line below to rotate the camera
@@ -78,15 +109,17 @@ public class ModelViewer extends Application3D {
         Shader.setViewMatrix(gl, view.getMatrix());
 
         // Set the lighting properties
-        Shader.setPoint3D(gl, "lightPos", new Point3D(0, 0, 5));
-        Shader.setColor(gl, "lightIntensity", Color.WHITE);
-        Shader.setColor(gl, "ambientIntensity", new Color(0.2f, 0.2f, 0.2f));
+        if (USE_LIGHTING) {
+            Shader.setPoint3D(gl, "lightPos", new Point3D(0, 0, 5));
+            Shader.setColor(gl, "lightIntensity", Color.WHITE);
+            Shader.setColor(gl, "ambientIntensity", new Color(0.2f, 0.2f, 0.2f));
 
-        // Set the material properties
-        Shader.setColor(gl, "ambientCoeff", Color.WHITE);
-        Shader.setColor(gl, "diffuseCoeff", new Color(0.5f, 0.5f, 0.5f));
-        Shader.setColor(gl, "specularCoeff", new Color(0.8f, 0.8f, 0.8f));
-        Shader.setFloat(gl, "phongExp", 16f);
+            // Set the material properties
+            Shader.setColor(gl, "ambientCoeff", Color.WHITE);
+            Shader.setColor(gl, "diffuseCoeff", new Color(0.5f, 0.5f, 0.5f));
+            Shader.setColor(gl, "specularCoeff", new Color(0.8f, 0.8f, 0.8f));
+            Shader.setFloat(gl, "phongExp", 16f);
+        }
 
         // The coordinate frame for both objects
         CoordFrame3D frame = CoordFrame3D.identity().translate(0, -0.5f, -2);
@@ -106,7 +139,6 @@ public class ModelViewer extends Application3D {
         // This translation and scale works well for the tree
 //           .translate(0,0.5f,0).scale(0.1f,0.1f,0.1f);
 
-        Shader.setPenColor(gl, new Color(0.5f, 0.5f, 0.5f));
         model.draw(gl, modelFrame);
 
         // A blue base for the model to sit on.
